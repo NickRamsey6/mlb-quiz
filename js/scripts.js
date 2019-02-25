2a21fb96a62d241d5099864e9ba10b7008e50838
Nick Ramsey <>
2019-02-24 22:47:19 -0800

    add final two questions and set default result

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,32 @@
-var team = "Dodgers";
+var team = "Cardinals";
 
 // console.log(team);
 
 $(document).ready(function() {
   $('form#mlbQuiz').submit(function(event) {
+    var age = parseInt($('input#age').val());
     var care = $("select#care").val();
     var pride = $("select#pride").val();
     var pc = $("select#pc").val();
+    var fight = $("input:radio[name=fight]:checked").val();
 
-
-    if (care ==='no') {
+    if(age>=50) {
+      var team ="Cubs";
+    }
+    else if (care ==='no') {
        var team = "Mariners";
     }
     else if (pride ==='yes') {
       var team = "Yankees";
     }
-    else if (pc === 'hate') {
+    else if (pc ==='hate') {
       var team = "Indians";
+    }
+    else if (fight ==='during') {
+      var team = "Dodgers";
+    }
+    else {
+      var team = "Rays"
     }
 
     $('#result').append(team);
